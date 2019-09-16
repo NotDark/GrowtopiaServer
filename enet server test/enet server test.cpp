40a3b8b211aa21cbe581f2a36f6455ebcb6d41e8
MarmoriPelaa <sup.maregames@gmail.com>
2019-09-16 23:36:08 +0300

    Update enet server test.cpp

--- enet server test/enet server test.cpp
+++ enet server test/enet server test.cpp
@@ -1632,7 +1632,7 @@ void SendPacketRaw(int a1, void *packetData, size_t packetDataSize, void *a4, EN
 				if (world->items[x + (y*world->width)].foreground == 758)
 					sendRoulete(peer, x, y);
 			}
-			else {
+			/*else*/ {
 				if (y < world->height)
 				{
 					world->items[x + (y*world->width)].breakTime = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
@@ -1648,7 +1648,7 @@ void SendPacketRaw(int a1, void *packetData, size_t packetDataSize, void *a4, EN
 					blockdef.blockType = BlockTypes::FOREGROUND;
 
 				}
-				if (y < world->height && world->items[x + (y*world->width)].breakLevel + 3 >= blockdef.breakHits * 3) { // TODO
+				if (y < world->height && world->items[x + (y*world->width)].breakLevel >= blockdef.breakHits * 3) { // TODO
 					data.packetType = 0x3;// 0xC; // 0xF // World::HandlePacketTileChangeRequest
 					data.netID = -1;
 					data.plantingTree = 0;
